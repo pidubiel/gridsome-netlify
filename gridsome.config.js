3c6a8a59ea3f180a4381778cfd6d4ba2f2b1043f
Piotr Dubiel <piotr.dubiel@bluepaprica.com>
2020-04-10 08:27:28 +0200

    add blog feature

--- gridsome.config.js
+++ gridsome.config.js
@@ -6,5 +6,22 @@
 
 module.exports = {
   siteName: 'Gridsome',
-  plugins: []
+  transformers: {
+    remark: {
+      externalLinksTarget: '_blank',
+      externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
+      anchorClassName: 'icon icon-link',
+    }
+  },
+
+  plugins: [
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        path: 'blog/**/*.md',
+        typeName: 'Post',
+        remark: {}
+      }
+    }
+  ]
 }
--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -2,11 +2,12 @@
   <div class="layout">
     <header class="header">
       <strong>
-        <g-link to="/">{{ $static.metadata.siteName }}</g-link>
+        <g-link to="/">{{ $static.metaData.siteName }}</g-link>
       </strong>
       <nav class="nav">
         <g-link class="nav__link" to="/">Home</g-link>
-        <g-link class="nav__link" to="/about/">About</g-link>
+        <g-link class="nav__link" to="/about">About</g-link>
+        <g-link class="nav__link" to="/team">Team</g-link>
       </nav>
     </header>
     <slot/>
@@ -14,37 +15,37 @@
 </template>
 
 <static-query>
-query {
-  metadata {
-    siteName
+  query {
+  metaData {
+  siteName
   }
-}
+  }
 </static-query>
 
 <style>
-body {
-  font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
-  margin:0;
-  padding:0;
-  line-height: 1.5;
-}
+  body {
+    font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
+    margin:0;
+    padding:0;
+    line-height: 1.5;
+  }
 
-.layout {
-  max-width: 760px;
-  margin: 0 auto;
-  padding-left: 20px;
-  padding-right: 20px;
-}
+  .layout {
+    max-width: 760px;
+    margin: 0 auto;
+    padding-left: 20px;
+    padding-right: 20px;
+  }
 
-.header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 20px;
-  height: 80px;
-}
+  .header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+    height: 80px;
+  }
 
-.nav__link {
-  margin-left: 20px;
-}
+  .nav__link {
+    margin-left: 20px;
+  }
 </style>
